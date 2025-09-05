e70a244d2c4245ae232a5a5bb85d2d81b1f3bd31
Skwiz-blip <louisskwiz@gmail.com>
2025-09-05 11:30:58 +0000

    Fix: Augmentation des budgets CSS pour Vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5dGdvY2hreGNkb2J4ZmZhY2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NDI3MzMsImV4cCI6MjA3MjMxODczM30.5O9XjQEevDocVCJJkUHISqpjhySSfum523E6_J9zZpw'
   },
   whatsapp: {
-    adminPhone: '+22891933619' // Format: +33123456789
+    adminPhone: '+22891933619'  
   }
 };
